5607680eede79f3dd10486df824cadc5113940f6
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-11 22:09:03 +0200

    - Add create_reply

--- src/server/Database/factories/create_reply.c
+++ src/server/Database/factories/create_reply.c
@@ -7,10 +7,51 @@
 
 #include "database.h"
 
-int create_reply(database_t *db, const char *msg, uuid_selector_t *params)
+static thread_t *get_thread_from_uuid(database_t *db,
+uuid_selector_t *params, int *err)
 {
-    (void) db;
-    (void) msg;
-    (void) params;
-    return 0;
+    thread_t *thread = NULL;
+    channel_t *channel = get_channel_from_uuid(db, params, err);
+
+    if (!channel)
+        return NULL;
+    LIST_FOREACH(thread, &channel->threads, entries) {
+        if (!uuid_compare(thread->uuid, params->uuid_thread))
+            return thread;
+    }
+    return NULL;
+}
+
+static int init_replay_node(thread_t *thread, user_t *user, const char *body)
+{
+    reply_t *reply = malloc(sizeof(reply_t));
+
+    if (!reply)
+        return ERROR;
+    memset(reply, 0, sizeof(reply_t));
+    memcpy(reply->body, body, strlen(body));
+    reply->timestamp = time(NULL);
+    reply->user = user;
+    uuid_generate(reply->uuid);
+    LIST_INSERT_HEAD(&thread->replies, reply, entries);
+    return SUCCESS;
+}
+
+int create_reply(database_t *db, const char *body, uuid_selector_t *params)
+{
+    int err = ERROR;
+    thread_t *thread = NULL;
+
+    if (!db || !body || !params)
+        return ERROR;
+    if (strlen(body) > SIZE_BODY)
+        return ERROR;
+    if (uuid_is_null(params->uuid_team) ||
+        uuid_is_null(params->uuid_channel) ||
+        uuid_is_null(params->uuid_thread))
+        return ERROR;
+    thread = get_thread_from_uuid(db, params, &err);
+    if (!thread)
+        return err;
+    return init_replay_node(thread, NULL, body);
 }
