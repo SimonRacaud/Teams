5883754e2d8a80b7ad20616f37eb740376d1313b
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-12 16:33:31 +0200

    - Add updater

--- src/server/Database/updater/user_subscription_add.c
+++ src/server/Database/updater/user_subscription_add.c
@@ -7,11 +7,22 @@
 
 #include "database.h"
 
+static int subscription_add(database_t *db, uuid_selector_t *params)
+{
+    team_t *team = get_team(db, params);
+    user_t *user = get_user(db, params);
+
+    if (!team || !user)
+        return (!team) ? ERR_UNKNOWN_TEAM : ERR_UNKNOWN_USER;
+    LIST_INSERT_HEAD(&user->teams, team, entries);
+    return SUCCESS;
+}
+
 int user_subscription_add(database_t *db, uuid_selector_t *params)
 {
     if (!db || !params)
         return ERROR;
     if (uuid_is_null(params->uuid_user) || uuid_is_null(params->uuid_team))
         return ERROR;
-    return SUCCESS;
+    return subscription_add(db, params);
 }
--- src/server/Database/updater/user_subscription_remove.c
+++ src/server/Database/updater/user_subscription_remove.c
@@ -7,11 +7,26 @@
 
 #include "database.h"
 
+static int subscription_remove(database_t *db, uuid_selector_t *params)
+{
+    team_t *team = NULL;
+    user_t *user = get_user(db, params);
+
+    if (!user)
+        return ERR_UNKNOWN_USER;
+    LIST_FOREACH(team, &user->teams, entries) {
+        if (!uuid_compare(team->uuid, params->uuid_team)) {
+            LIST_REMOVE(team, entries);
+        }
+    }
+    return SUCCESS;
+}
+
 int user_subscription_remove(database_t *db, uuid_selector_t *params)
 {
     if (!db || !params)
         return ERROR;
     if (uuid_is_null(params->uuid_user) || uuid_is_null(params->uuid_team))
         return ERROR;
-    return SUCCESS;
+    return subscription_remove(db, params);
 }
