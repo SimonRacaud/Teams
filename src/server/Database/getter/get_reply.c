ee4a8a0e34d4446a34f2ee746111258939e61512
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-12 10:02:22 +0200

    - Add getter

--- src/server/Database/getter/get_reply.c
+++ src/server/Database/getter/get_reply.c
@@ -7,9 +7,29 @@
 
 #include "database.h"
 
+static reply_t *get_reply_from_uuid(database_t *db, uuid_selector_t *params)
+{
+    reply_t *reply = NULL;
+    thread_t *thread = get_thread(db, params);
+
+    if (!thread)
+        return NULL;
+    LIST_FOREACH(reply, &thread->replies, entries) {
+        if (!uuid_compare(reply->uuid, params->uuid_reply))
+            return reply;
+    }
+    return NULL;
+}
+
 reply_t *get_reply(database_t *db, uuid_selector_t *params)
 {
-    (void) db;
-    (void) params;
-    return NULL;
+    if (!db || !params)
+        return NULL;
+    if (uuid_is_null(params->uuid_team) ||
+        uuid_is_null(params->uuid_channel) ||
+        uuid_is_null(params->uuid_thread) ||
+        uuid_is_null(params->uuid_reply)
+    )
+        return NULL;
+    return get_reply_from_uuid(db, params);
 }
