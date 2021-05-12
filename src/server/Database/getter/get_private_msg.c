48bab001e2132f640a908575d8843ec17706394b
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-12 10:08:31 +0200

    - Add get_private_msg

--- src/server/Database/getter/get_private_msg.c
+++ src/server/Database/getter/get_private_msg.c
@@ -7,9 +7,27 @@
 
 #include "database.h"
 
+static private_msg_t *get_private_msg_from_uuid(
+    database_t *db, uuid_selector_t *params)
+{
+    private_msg_t *msg = NULL;
+    user_t *user = get_user(db, params);
+
+    if (!user)
+        return NULL;
+    LIST_FOREACH(msg, &user->messages, entries) {
+        if (!uuid_compare(msg->uuid, params->uuid_private_msg))
+            return msg;
+    }
+    return NULL;
+}
+
 private_msg_t *get_private_msg(database_t *db, uuid_selector_t *params)
 {
-    (void) db;
-    (void) params;
-    return NULL;
+    if (!db || !params)
+        return NULL;
+    if (uuid_is_null(params->uuid_user) ||
+        uuid_is_null(params->uuid_private_msg))
+        return NULL;
+    return get_private_msg_from_uuid(db, params);
 }
