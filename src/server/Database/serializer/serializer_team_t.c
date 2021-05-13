e595a359bfaa276d3f86344734a43dafde5e6907
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-13 10:03:50 +0200

    - Add serializer_team_t

--- src/server/Database/serializer/serializer_team_t.c
+++ src/server/Database/serializer/serializer_team_t.c
@@ -5,11 +5,23 @@
 ** serializer_team_t function
 */
 
+#include <stdlib.h>
+#include <string.h>
 #include "data/team_t.h"
 #include "save/bin_team_t.h"
 
 bin_team_t *serializer_team_t(team_t *src)
 {
-    (void) src;
-    return NULL;
+    bin_team_t *dest = NULL;
+
+    if (!src)
+        return NULL;
+    dest = malloc(sizeof(bin_team_t));
+    if (!dest)
+        return NULL;
+    memset(dest, 0, sizeof(bin_team_t));
+    uuid_copy(dest->uuid, src->uuid);
+    memcpy(dest->name, src->name, strlen(src->name));
+    memcpy(dest->description, src->description, strlen(src->description));
+    return dest;
 }
