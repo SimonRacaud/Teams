f8e3ce552311ab378151918c07b14d97938b0a83
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-13 12:22:21 +0200

    feat(include): selected_entity_t

---
+++ include/client/selected_entity_t.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** B-NWP-400-REN-4-1-myteams-simon.racaud
+** File description:
+** 13/05/2021 selected_entity_t.h
+*/
+
+#ifndef SELECTED_ENTITY_T_H
+#define SELECTED_ENTITY_T_H
+
+#include <uuid/uuid.h>
+
+typedef struct selected_entity_s {
+    uuid_t team;
+    uuid_t channel;
+    uuid_t thread;
+} selected_entity_t;
+
+#endif // SELECTED_ENTITY_T_H
