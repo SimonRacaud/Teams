5a3f01a23c4377ec8230fbc0e6c4988d59f34ec2
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-13 12:51:30 +0200

    chore(client): logger body

---
+++ src/client/logger/logger.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** B-NWP-400-REN-4-1-myteams-simon.racaud
+** File description:
+** 13/05/2021 logger.c
+*/
+
+#include "client.h"
+#include "logging_client.h"
+
+int logger(response_t *response)
+{
+
+    return EXIT_SUCCESS;
+}
