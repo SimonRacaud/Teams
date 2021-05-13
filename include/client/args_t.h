769cbf3ad5ff4fc59c09e6a2ec3132e852fbe544
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-13 12:26:47 +0200

    feat(include/client): args_t

---
+++ include/client/args_t.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** B-NWP-400-REN-4-1-myteams-simon.racaud
+** File description:
+** 13/05/2021 args_t.h
+*/
+
+#ifndef ARGS_T_H
+#define ARGS_T_H
+
+typedef struct args_s {
+    uint port;
+    char *ip;
+} args_t;
+
+#endif // ARGS_T_H
