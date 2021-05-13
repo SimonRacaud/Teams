217e3e5204b4659b5a11c2fc1f02ff43412bdda5
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-13 12:27:48 +0200

    feat(client/init): parse_args

---
+++ src/client/init/parse_args.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2021
+** B-NWP-400-REN-4-1-myteams-simon.racaud
+** File description:
+** 13/05/2021 parse_args.c
+*/
+
+#include "client.h"
+
+static const int REQUIRED_ARGC = 3;
+static const char *HELP = "-help";
+
+static int usage(const char *bin, int code)
+{
+    printf("USAGE:\t%s ip port\n", bin);
+    printf("\tip\tis the server ip address on which the server socket"
+           " listens\n");
+    printf("\tport\tis the port number on which the server socket listens\n");
+    return code;
+}
+
+int parse_args(args_t *args, int argc, char **argv)
+{
+    for (size_t i = 0; i < (size_t)argc; i++) {
+        if (strcmp(argv[i], HELP) == 0) {
+            exit(usage(argv[0], EXIT_SUCCESS));
+        }
+    }
+    if (argc != REQUIRED_ARGC) {
+        return usage(argv[0], EXIT_FAILURE);
+    }
+    if (!is_number(argv[1]) || !is_ip(argv[2])) {
+        return EXIT_FAILURE;
+    }
+    args->port = (uint)atoi(argv[1]);
+    args->ip = argv[2];
+    return EXIT_SUCCESS;
+}
