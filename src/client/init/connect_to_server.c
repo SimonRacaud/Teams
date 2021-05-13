c6e8809753343753a3965a6c379f9f8dce763df0
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-13 12:27:23 +0200

    feat(client): connect_to_server function

---
+++ src/client/init/connect_to_server.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** B-NWP-400-REN-4-1-myteams-simon.racaud
+** File description:
+** 13/05/2021 connection_to_server.c
+*/
+
+#include "client.h"
+
+int connect_to_server(socket_t *sock, uint port, const char *ip)
+{
+    for (size_t attempt = 0; attempt < CONNECT_ATTEMPT; attempt++) {
+        if (socket_client_ip_connect(sock, port, ip) == EXIT_SUCCESS) {
+            return EXIT_SUCCESS;
+        }
+    }
+    printf("Too many attempt. quit\n");
+    return EXIT_FAILURE;
+}
