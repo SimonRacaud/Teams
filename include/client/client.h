d3540a30cb81616b76de53952451156120b3ec7e
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-13 12:28:41 +0200

    chore(include/client): update prototypes

--- include/client/client.h
+++ include/client/client.h
@@ -12,5 +12,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/queue.h>
+
+#include "socket.h"
+#include "client_t.h"
+#include "utility.h"
+#include "network/response_t.h"
+
+#define CONNECT_ATTEMPT 10
+#define MY_LIST_HEAD_INITIALIZER(head) head.slh_first = NULL
+
+/// INIT
+int app_init(client_t *client, int argc, char **argv);
+int connect_to_server(socket_t *sock, uint port, const char *ip);
+int parse_args(args_t *args, int argc, char **argv);
+
+/// DESTROY
+int app_destroy(client_t *client);
+
+/// LOOP
+int app_loop(client_t *client);
+int process_write(client_t *client);
+int process_read(client_t *client);
+char *prompt(char **buffer_ptr, bool *disconnect);
+
+/// LOGGER
+int logger(response_t *response);
 
 #endif /* !APP_H_ */
