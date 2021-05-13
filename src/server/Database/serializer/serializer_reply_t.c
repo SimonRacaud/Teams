d955cc970f337267e258554015e6cbebf04d7bf5
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-13 10:14:05 +0200

    - Add serializer_reply_t (+ TODO)

--- src/server/Database/serializer/serializer_reply_t.c
+++ src/server/Database/serializer/serializer_reply_t.c
@@ -5,11 +5,26 @@
 ** serializer_reply_t function
 */
 
+#include <stdlib.h>
+#include <string.h>
 #include "data/reply_t.h"
 #include "save/bin_reply_t.h"
 
 bin_reply_t *serializer_reply_t(reply_t *src)
 {
-    (void) src;
-    return NULL;
+    bin_reply_t *dest = NULL;
+
+    if (!src)
+        return NULL;
+    dest = malloc(sizeof(bin_reply_t));
+    if (!dest)
+        return NULL;
+    memset(dest, 0, sizeof(bin_reply_t));
+    dest->timestamp = src->timestamp;
+    uuid_copy(dest->uuid, src->uuid);
+    uuid_copy(dest->user_uuid, src->user->uuid);
+    // TODO set the vqlue of dest->thread_uuid
+    // uuid_copy(dest->thread_uuid, ?);
+    memcpy(dest->body, src->body, strlen(src->body));
+    return dest;
 }
