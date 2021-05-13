3f09d766424dec0f7b1327d8987205acd36fd963
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-13 10:23:11 +0200

    - Add serializer_private_msg_t

--- src/server/Database/serializer/serializer_private_msg_t.c
+++ src/server/Database/serializer/serializer_private_msg_t.c
@@ -5,11 +5,25 @@
 ** serializer_private_msg_t function
 */
 
+#include <stdlib.h>
+#include <string.h>
 #include "data/private_msg_t.h"
 #include "save/bin_private_msg_t.h"
 
-bin_private_msg_t *serializer_private_msg_t(private_msg_t *src)
+bin_private_msg_t *serializer_private_msg_t(const private_msg_t *src)
 {
-    (void) src;
-    return NULL;
+    bin_private_msg_t *dest = NULL;
+
+    if (!src)
+        return NULL;
+    dest = malloc(sizeof(bin_private_msg_t));
+    if (!dest)
+        return NULL;
+    memset(dest, 0, sizeof(bin_private_msg_t));
+    dest->timestamp = src->timestamp;
+    uuid_copy(dest->uuid, src->uuid);
+    uuid_copy(dest->receiver_uuid, src->receiver->uuid);
+    uuid_copy(dest->sender_uuid, src->sender->uuid);
+    memcpy(dest->body, src->body, strlen(src->body));
+    return dest;
 }
