63b0eb1090d7f1e9341ee8f8701dc99e18220512
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-13 10:28:36 +0200

    - Add serializer_channel_t

--- src/server/Database/serializer/serializer_channel_t.c
+++ src/server/Database/serializer/serializer_channel_t.c
@@ -5,11 +5,25 @@
 ** serializer_channel_t function
 */
 
+#include <stdlib.h>
+#include <string.h>
 #include "data/channel_t.h"
 #include "save/bin_channel_t.h"
 
-bin_channel_t *serializer_channel_t(channel_t *src)
+bin_channel_t *serializer_channel_t(const channel_t *src)
 {
-    (void) src;
-    return NULL;
+    bin_channel_t *dest = NULL;
+
+    if (!src)
+        return NULL;
+    dest = malloc(sizeof(bin_channel_t));
+    if (!dest)
+        return NULL;
+    memset(dest, 0, sizeof(bin_channel_t));
+    uuid_copy(dest->uuid, src->uuid);
+    // TODO set the vqlue of dest->team_uuid
+    // uuid_copy(dest->team_uuid, ?);
+    memcpy(dest->name, src->name, strlen(src->name));
+    memcpy(dest->description, src->description, strlen(src->description));
+    return dest;
 }
