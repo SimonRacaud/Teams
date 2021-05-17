2e620bc901beae6f83f29ca0b9ea680aedaf45bc
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-17 09:39:52 +0200

    - Add TODO in deserializer

--- src/server/Database/deserializer/deserializer_channel_t.c
+++ src/server/Database/deserializer/deserializer_channel_t.c
@@ -15,6 +15,7 @@ channel_t *deserializer_channel_t(
     const bin_channel_t *src, const database_t *db)
 {
     channel_t *dest = NULL;
+    uuid_selector_t params = {0};
 
     if (!src || !db)
         return NULL;
@@ -22,7 +23,12 @@ channel_t *deserializer_channel_t(
     if (!dest)
         return NULL;
     memset(dest, 0, sizeof(channel_t));
+    memset(&params, 0, sizeof(uuid_selector_t));
     uuid_copy(dest->uuid, src->uuid);
+    uuid_copy(params.uuid_team, src->team_uuid);
+    dest->parent_team = get_team(db, &params);
+    if (!dest->parent_team)
+        return NULL;
     memcpy(dest->name, src->name, strlen(src->name));
     memcpy(dest->description, src->description, strlen(src->description));
     return dest;
