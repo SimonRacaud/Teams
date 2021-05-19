58823d38d378406040b198825f5a60e7d0153c6d
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-19 09:16:45 +0200

    refactor(server/handler): simplify help and user commands response management

--- src/server/handler/handler_help.c
+++ src/server/handler/handler_help.c
@@ -30,42 +30,14 @@ static char *get_help_msg(void)
     return list;
 }
 
-static void *get_body(void)
-{
-    body_header_t body_struct = {0};
-    const char type[] = "string";
-    char *msg = get_help_msg();
-    void *body = NULL;
-
-    if (!msg)
-        return NULL;
-    body = malloc(sizeof(body_header_t) + strlen(msg) + 1);
-    if (!body)
-        return NULL;
-    body_struct.list_size = 1;
-    body_struct.elem_size = strlen(msg) + 1;
-    memcpy(body_struct.type, type, strlen(type));
-    memcpy(body, &body_struct, sizeof(body_header_t));
-    memcpy(body + sizeof(body_header_t), msg, body_struct.elem_size);
-    free(msg);
-    return body;
-}
-
 int handler_help(server_t *srv, request_t *request, UNUSED client_t *client)
 {
-    response_t *response = NULL;
-    int return_value = 0;
-    void *body = NULL;
+    char *content = NULL;
 
     if (!srv || !request)
         return EXIT_FAILURE;
-    body = get_body();
-    if (!body)
+    content = get_help_msg();
+    if (!content)
         return EXIT_FAILURE;
-    response = response_create(SUCCESS, request, request->receiver, body);
-    if (!response)
-        return EXIT_FAILURE;
-    return_value = response_send(response);
-    response_destroy(response);
-    return return_value;
+    return reply_str(SUCCESS, request, content);
 }
--- src/server/handler/handler_user.c
+++ src/server/handler/handler_user.c
@@ -7,87 +7,34 @@
 
 #include "server.h"
 #include "database.h"
+#include "utility.h"
 #include "request_handler_t.h"
 
-static bool is_correct_arg(database_t *db,
-request_t *req, int *err, user_t **user)
+static int send_result(user_t *user, request_t *request)
 {
-    uuid_t uuid;
+    void *body = body_maker_user(user, false);
 
-    if (get_arg_size((const char **) req->args) == 1) {
-        if (strlen(req->args[0]) + 1 == 16) {
-            get_uuid_from_string(uuid, req->args[0]);
-            *user = get_user_from_uuid(db, uuid);
-            *err = ERR_UNKNOWN_USER;
-            return (*user) ? true : false;
-        } else {
-            *err = ERROR;
-            return false;
-        }
-    } else {
-        *err = ERROR;
-        return false;
-    }
-}
-
-static char *get_users_data(user_t *user)
-{
-    char *str = NULL;
-
-    asprintf(&str, "Username: %s\nUuid: %s", user->username, user->uuid);
-    return str;
-}
-
-static void *get_body(user_t *user)
-{
-    body_header_t body_struct = {0};
-    const char type[] = "string";
-    char *data = get_users_data(user);
-    void *body = NULL;
-
-    if (!data)
-        return NULL;
-    body = malloc(sizeof(body_header_t) + strlen(data) + 1);
     if (!body)
-        return NULL;
-    body_struct.list_size = 1;
-    body_struct.elem_size = strlen(data) + 1;
-    memcpy(body_struct.type, type, strlen(type));
-    memcpy(body, &body_struct, sizeof(body_header_t));
-    memcpy(body + sizeof(body_header_t), data, body_struct.elem_size);
-    free(data);
-    return body;
-}
-
-static response_t *get_response(server_t *srv, request_t *request)
-{
-    user_t *user = NULL;
-    response_t *response = NULL;
-    int err = ERROR;
-    void *body = NULL;
-
-    if (is_correct_arg(&srv->database, request, &err, &user)) {
-        body = get_body(user);
-        if (!body)
-            return NULL;
-        response = response_create(SUCCESS, request, request->receiver, body);
-    } else {
-        response = response_create(err, request, request->receiver, NULL);
-    }
-    return response;
+        return EXIT_FAILURE;
+    return reply(SUCCESS, request, body);
 }
 
 int handler_user(server_t *srv, request_t *request, UNUSED client_t *client)
 {
-    response_t *response = NULL;
-    int return_value = 0;
+    uuid_selector_t select = {0};
+    user_t *user = NULL;
 
     if (!srv || !request)
         return EXIT_FAILURE;
-    response = get_response(srv, request);
-    if (!response)
-        return EXIT_FAILURE;
-    return_value = response_send(response);
-    response_destroy(response);
-    return return_value;
+    if (walen(request->args) != 1) {
+        return reply_str(ERROR, request, "Invalid argument count");
+    }
+    if (uuid_parse(request->args[0], select.uuid_user) == -1) {
+        return reply_str(ERROR, request, "Invalid argument");
+    }
+    user = get_user(&srv->database, &select);
+    if (!user) {
+        return reply_str(ERR_UNKNOWN_USER, request, "Unknown user");
+    }
+    return send_result(user, request);
 }
