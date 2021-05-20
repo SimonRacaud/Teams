9bb4459876f9aada51dc36238f64cd8326d793ae
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-05-20 17:23:53 +0200

    Fix laod database in correct order

--- src/server/Database/deserializer/deserializer_user_t.c
+++ src/server/Database/deserializer/deserializer_user_t.c
@@ -30,26 +30,6 @@ user_t *deserializer_user_t(const bin_user_t *src, const database_t *db)
     return dest;
 }
 
-static bool fill_user_teams(const database_save_t *db_save, database_t *db,
-    user_t *user, uint user_index)
-{
-    team_t *team;
-    uint teams_index = 0;
-    uuid_selector_t params = {0};
-
-    memset(&params, 0, sizeof(uuid_selector_t));
-    for (uint k = 0; k < db_save->users[user_index]->nb_subscribed_teams;
-         k++, teams_index++) {
-        uuid_copy(params.uuid_team, *db_save->user_teams_list[teams_index]);
-        team = get_team(db, &params);
-        if (team == NULL)
-            return false;
-        LIST_INSERT_HEAD(&team->users, user, entries);
-        LIST_INSERT_HEAD(&user->teams, team, entries);
-    }
-    return true;
-}
-
 static bool fill_user_private_msg(
     const database_save_t *db_save, database_t *db)
 {
@@ -75,8 +55,6 @@ bool deserialize_all_users(const database_save_t *db_save, database_t *db)
     for (uint i = 0; i < db_save->head->nb_user; i++) {
         user = deserializer_user_t(db_save->users[i], db);
         if (user == NULL)
-            return false;
-        if (!fill_user_teams(db_save, db, user, i))
             return false;
         LIST_INSERT_HEAD(&db->users, user, entries);
     }
--- src/server/Database/loader/convert_database.c
+++ src/server/Database/loader/convert_database.c
@@ -21,15 +21,48 @@ database_t *create_empty_database(void)
     return db;
 }
 
+static bool fill_user_teams(const database_save_t *db_save, database_t *db,
+    user_t *user, uint user_index)
+{
+    team_t *team;
+    uuid_selector_t params = {0};
+
+    bzero(&params, sizeof(uuid_selector_t));
+    for (uint k = 0; k < db_save->users[user_index]->nb_subscribed_teams; k++) {
+        uuid_copy(params.uuid_team, db_save->user_teams_list[user_index][k]);
+        team = get_team(db, &params);
+        if (team == NULL)
+            return false;
+        LIST_INSERT_HEAD(&team->users, user, entries);
+        LIST_INSERT_HEAD(&user->teams, team, entries);
+    }
+    return true;
+}
+
+static bool get_users_to_fill_teams(
+    const database_save_t *db_save, database_t *db)
+{
+    user_t *user;
+
+    for (uint i = 0; i < db_save->head->nb_user; i++) {
+        user = get_user_from_uuid(db, db_save->users[i]->uuid);
+        if (!fill_user_teams(db_save, db, user, i))
+            return false;
+    }
+    return true;
+}
+
 database_t *convert_saved_db_to_release_db(const database_save_t *db_save)
 {
     database_t *db = create_empty_database();
 
     if (db == NULL)
         return NULL;
+    if (!deserialize_all_users(db_save, db))
+        return NULL;
     if (!deserialize_all_teams(db_save, db))
         return NULL;
-    if (!deserialize_all_users(db_save, db))
+    if (!get_users_to_fill_teams(db_save, db))
         return NULL;
     return db;
 }
