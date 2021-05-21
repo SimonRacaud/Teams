56ac7b6118c2362b34f96f1bb571f763d6ba330c
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-21 16:31:40 +0200

    feat(client/logger): create loggin handler type macro

--- include/client/logger.h
+++ include/client/logger.h
@@ -15,6 +15,21 @@ typedef struct body_handler_s {
     void (* handler)(response_t *response);
 } body_handler_t;
 
+#define LOG_T_STRING "string"
+#define LOG_T_LOGGING "logging"
+#define LOG_T_EVT_TEAM "event_team"
+#define LOG_T_EVT_REPLY "event_reply"
+#define LOG_T_EVT_CHAN "event_channel"
+#define LOG_T_EVT_THREAD "event_thread"
+#define LOG_T_PRT_USER "print_user"
+#define LOG_T_PRT_TEAM "print_team"
+#define LOG_T_PRT_CHAN "print_channel"
+#define LOG_T_PRT_THREAD "print_thread"
+#define LOG_T_PRT_REPLY "print_reply"
+#define LOG_T_PRT_PRIV_MSG "print_private_msg"
+#define LOG_T_ERROR "error"
+#define LOG_T_SUBSCRIPTION "subscription"
+
 int logger(response_t *response);
 
 void log_string(response_t *response);
