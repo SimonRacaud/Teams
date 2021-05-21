7138bf4487822734cc566fe1941ec069d3ba5003
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-21 16:57:21 +0200

    fix(server): avoid server to exit failure on sigint, add debug print on failure

--- src/server/app/app_create.c
+++ src/server/app/app_create.c
@@ -19,12 +19,18 @@ static void client_list_init(server_t *server)
 
 int app_create(server_t *server, uint port)
 {
-    if (signal_manager(SIGINT, &server->loop) == EXIT_FAILURE)
+    if (signal_manager(SIGINT, &server->loop) == EXIT_FAILURE) {
+        printf("signal_manager: fail\n");
         return EXIT_FAILURE;
-    if (socket_server_create(&server->socket, port, SERVER_MAX_CLIENT))
+    }
+    if (socket_server_create(&server->socket, port, SERVER_MAX_CLIENT)) {
+        printf("socket_server_create: fail\n");
         return EXIT_FAILURE;
+    }
     client_list_init(server);
-    if (!load_database(&server->database))
+    if (!load_database(&server->database)) {
+        printf("load_database: fail\n");
         return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
