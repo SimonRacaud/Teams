ff2b96daff2a136715f71d8c5f36346be67880fd
RACAUD Simon <simon.racaud@epitech.eu>
2021-05-22 17:45:02 +0200

    feat(server/handler/info): update responses

--- src/server/handler/handler_info.c
+++ src/server/handler/handler_info.c
@@ -23,10 +23,8 @@ static int info_thread_manage(
     uuid_copy(params.uuid_thread, client->selector.thread);
     thread = get_thread(&srv->database, &params);
     if (!thread)
-        return EXIT_FAILURE;
+        return reply_error(ERR_UNKNOWN_THREAD, request, params.uuid_thread);
     body = body_maker_thread(thread, false, LOG_T_PRT_THREAD);
-    if (!body)
-        return EXIT_FAILURE;
     return reply(SUCCESS, request, body, NULL);
 }
 
@@ -42,10 +40,8 @@ static int info_channel_manage(
     uuid_copy(params.uuid_channel, client->selector.channel);
     channel = get_channel(&srv->database, &params);
     if (!channel)
-        return EXIT_FAILURE;
+        return reply_error(ERR_UNKNOWN_CHANNEL, request, params.uuid_channel);
     body = body_maker_channel(channel, false, LOG_T_PRT_CHAN);
-    if (!body)
-        return EXIT_FAILURE;
     return reply(SUCCESS, request, body, NULL);
 }
 
@@ -60,10 +56,8 @@ static int info_team_manage(
     uuid_copy(params.uuid_team, client->selector.team);
     team = get_team(&srv->database, &params);
     if (!team)
-        return EXIT_FAILURE;
+        return reply_error(ERR_UNKNOWN_TEAM, request, params.uuid_team);
     body = body_maker_team(team, false, LOG_T_PRT_TEAM);
-    if (!body)
-        return EXIT_FAILURE;
     return reply(SUCCESS, request, body, NULL);
 }
 
@@ -72,8 +66,6 @@ static int info_user_manage(
 {
     void *body = body_maker_user(client->user_ptr, false, LOG_T_PRT_USER);
 
-    if (!body)
-        return EXIT_FAILURE;
     return reply(SUCCESS, request, body, NULL);
 }
 
