f929a3d1a9a40c0b6f851844a3d86ce2e89b9d56
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-05-25 12:36:10 +0200

    Fix segv on serializing

--- src/server/database/serializer/serializer_private_msg_t.c
+++ src/server/database/serializer/serializer_private_msg_t.c
@@ -5,10 +5,9 @@
 ** serializer_private_msg_t function
 */
 
-#include <stdlib.h>
-#include <string.h>
 #include "data/private_msg_t.h"
 #include "save/bin_private_msg_t.h"
+#include "database.h"
 
 bin_private_msg_t *serializer_private_msg_t(const private_msg_t *src)
 {
@@ -19,11 +18,13 @@ bin_private_msg_t *serializer_private_msg_t(const private_msg_t *src)
     dest = malloc(sizeof(bin_private_msg_t));
     if (!dest)
         return NULL;
-    memset(dest, 0, sizeof(bin_private_msg_t));
+    bzero(dest, sizeof(bin_private_msg_t));
     dest->timestamp = src->timestamp;
     uuid_copy(dest->uuid, src->uuid);
-    uuid_copy(dest->receiver_uuid, src->receiver->uuid);
-    uuid_copy(dest->sender_uuid, src->sender->uuid);
+    if (src->receiver->uuid)
+        uuid_copy(dest->receiver_uuid, src->receiver->uuid);
+    if (src->sender->uuid)
+        uuid_copy(dest->sender_uuid, src->sender->uuid);
     memcpy(dest->body, src->body, strlen(src->body));
     return dest;
 }
